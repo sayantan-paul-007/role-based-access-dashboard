c203a6195d34ddd7c87f0dde24f849b8520e52ba
Sayantan Paul <sayantanpaul222@gmail.com>
2025-05-19 18:05:16 +0530

    Front End structure done

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+import Logo from "@/components/logo";
 import Themetoggle from "@/components/theme-toggle";
+import Link from "next/link";
 
 export default function Home() {
 return(
-  <main>
-    <div className="flex bg-background dark:bg-dark-background h-full">
-
+  <main className="bg-background dark:bg-dark-background h-screen">
+    <div className="flex px-6 py-4 justify-between">
+      <Logo />
+       <Themetoggle />
     </div>
-  <h1 className="bg-foreground dark:bg-black">This is home page</h1>
-  <Themetoggle />
+  <h1 className="text-center">This is home page</h1>
+  <div className="flex items-center justify-center">
+    <Link href='/login'>
+      <button className="bg-accent py-2 px-5 rounded-md">
+        Login
+      </button>
+      </Link>
+  </div>
+      
   </main>
 )  
 }
